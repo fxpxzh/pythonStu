6c2c501920661e2a2e6b242c3214c3617064413d
fxpxzh <853313433@qq.com>
2016-04-01 14:22:03 +0800

    链式调用含参数

--- PythonTest/src/com/fangxp/oop_basic/special_userchain.py
+++ PythonTest/src/com/fangxp/oop_basic/special_userchain.py
@@ -1,15 +1,12 @@
-from pyclbr import Function
 class Chain(object):
     
     def user(self,name):
-        self._name = name 
+        return Chain('%s/%s' % (self._path, name))
     
     def __init__(self, path='users'):
         self._path = path
 
     def __getattr__(self, path):
-        if isinstance(path, Function):
-            return Chain('%s/%s'%(self._path,))
         return Chain('%s/%s' % (self._path, path))
 
     def __str__(self):
@@ -18,6 +15,6 @@ class Chain(object):
     __repr__ = __str__
 
 #链式调用
-c = Chain().status.user.timeline.list
+c = Chain().user("Michael").repos
 print(c)
 
